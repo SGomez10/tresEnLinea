6b93944399f993e3c6df06cd648198ee1ea9adef
aarriazu <aarriazu@elpuig.xeill.net>
2024-04-17 18:02:20 +0200

    Tarea 1: Cal crear un menú principal

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
-        System.out.println("Cambio1");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        while (1>0){
+            System.out.println("1. Nova partida\n2. Carregar partida\n3. Configuració\n4. Sortir");
+            char input = sc.next().charAt(0);
+            switch (input){
+                case '1':
+                    System.out.println("1. Nova partida");
+                    break;
+                case '2':
+                    System.out.println("2. Carregar partida");
+                    break;
+                case '3':
+                    System.out.println("3. Configuració");
+                    break;
+                case '4':
+                    System.out.println("4. Sortir");
+                    break;
+                default:
+                    break;
+            }
+            System.out.println();
+        }
     }
 }
