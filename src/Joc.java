22bc7499ae4add0351255b50dbfe7f6bd2631af9
SGomez10 <sgomez@elpuig.xeill.net>
2024-04-24 17:50:04 +0200

    Commit con el esqueleto de la clase Joc (abierto a cambios)

---
+++ src/Joc.java
@@ -0,0 +1,29 @@
+public class Joc {
+
+    char [][] taulell = new char[3][3];
+    short torn;
+
+    public char[][] getTaulell() {
+        return taulell;
+    }
+
+    public short getTorn() {
+        return torn;
+    }
+
+    public char[][] novaPartida(){
+
+        return taulell;
+    }
+
+    public void jugar(short fila, short columna ){
+
+    }
+
+    public boolean jugadaGuanyadora(short fila, short columna){
+
+
+        return false;
+    }
+
+}
